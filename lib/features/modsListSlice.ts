107ff5317d224e7f8e8a42a6de67cb8775e8fa45
ImDarkly <svit.darkly@gmail.com>
2025-12-29 00:56:41 +0200

    chore: refactor mods list

---
+++ lib/features/modsListSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	value: "",
+};
+
+const modsListSlice = createSlice({
+	name: "modsList",
+	initialState,
+	reducers: {
+		setModsList: (state, action) => {
+			state.value = action.payload;
+		},
+	},
+});
+
+export const setModsList = modsListSlice.actions;
+
+export default modsListSlice.reducer;
