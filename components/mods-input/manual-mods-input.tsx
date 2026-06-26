d34bcb4f21cfa523eca89c81100dee985d9ed65e
ImDarkly <svit.darkly@gmail.com>
2026-06-26 16:23:59 +0300

    Add debounced updates to manual mods input

--- components/mods-input/manual-mods-input.tsx
+++ components/mods-input/manual-mods-input.tsx
@@ -3,39 +3,51 @@ import { Textarea } from "../ui/textarea";
 import { setModNamesList } from "@/lib/slices/modNamesListSlice";
 import { useAppDispatch, useAppSelector } from "@/hooks/store";
 import { useEffect, useState } from "react";
+import { useDebounceCallback } from "@/hooks/useDebounceCallback";
 
 export default function ManualModsInput() {
-	const dispatch = useAppDispatch();
-	const modsList = useAppSelector((state) => state.modNamesList.modNames);
-	const [localValue, setLocalValue] = useState(modsList.join("\n"));
+  const dispatch = useAppDispatch();
+  const modsList = useAppSelector((state) => state.modNamesList.modNames);
+  const [localValue, setLocalValue] = useState(modsList.join("\n"));
 
-	useEffect(() => {
-		setLocalValue(modsList.join("\n"));
-	}, [modsList]);
+  const debouncedDispatch = useDebounceCallback((value: string) => {
+    const lines = value
+      .split("\n")
+      .map((line) => line.trim())
+      .filter(Boolean);
+    dispatch(setModNamesList(lines));
+  }, 500);
 
-	const handleBlur = () => {
-		const lines = localValue
-			.split("\n")
-			.map((line) => line.trim())
-			.filter(Boolean);
+  useEffect(() => {
+    setLocalValue(modsList.join("\n"));
+  }, [modsList]);
 
-		dispatch(setModNamesList(lines));
-	};
+  const handleBlur = () => {
+    const lines = localValue
+      .split("\n")
+      .map((line) => line.trim())
+      .filter(Boolean);
 
-	return (
-		<Field>
-			<FieldLabel htmlFor="mods-list">Manual Input</FieldLabel>
-			<Textarea
-				id="mods-list"
-				rows={10}
-				className="h-24 resize-none"
-				value={localValue}
-				onChange={(e) => setLocalValue(e.target.value)}
-				onBlur={handleBlur}
-			/>
-			<FieldDescription>
-				Enter the list of mods you want to include, one per line.
-			</FieldDescription>
-		</Field>
-	);
+    dispatch(setModNamesList(lines));
+  };
+
+  return (
+    <Field>
+      <FieldLabel htmlFor="mods-list">Manual Input</FieldLabel>
+      <Textarea
+        id="mods-list"
+        rows={10}
+        className="h-24 resize-none"
+        value={localValue}
+        onChange={(e) => {
+          setLocalValue(e.target.value);
+          debouncedDispatch(e.target.value);
+        }}
+        onBlur={handleBlur}
+      />
+      <FieldDescription>
+        Enter the list of mods you want to include, one per line.
+      </FieldDescription>
+    </Field>
+  );
 }
